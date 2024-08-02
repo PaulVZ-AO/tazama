9df6a449e85c35b5295d07130fff08dded21d5a2
Oliver Vermeulen <127955247+Oliver-AO-GH@users.noreply.github.com>
2024-08-02 15:48:08 +0200

    update

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -216,7 +216,7 @@ export function Modal(props: Props) {
                               <label htmlFor={`modal-Account-Number-${index}`}>Account Name</label>
                               <input type="text" id={`modal-Account-Number-${index}`} className="w-full rounded-lg bg-gray-200 p-2 shadow-inner"
                                 defaultValue={accountDetail.DbtrAcct.Nm}
-                                value={customAccount?.DbtrAcct.Nm}
+                                // value={customAccount?.DbtrAcct.Nm}
                                 onChange={(e) => {
                                   setCustomAccount({
                                     ...customAccount,
@@ -243,30 +243,25 @@ export function Modal(props: Props) {
                   </div>
                   
                   <div className="flex">
-
-
-
                     <button
-  type="button"
-  className="m-5 w-full rounded-lg bg-gradient-to-r from-gray-200 to-gray-100 py-2 shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)] hover:shadow-inner"
-  onClick={async () => {
-    console.log(customAccount);
+                      type="button"
+                      className="m-5 w-full rounded-lg bg-gradient-to-r from-gray-200 to-gray-100 py-2 shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)] hover:shadow-inner"
+                      onClick={async () => {
+                        console.log(customAccount);
 
-    if (accountDetails.length > 0 && typeof props.selectedEntity === "number") {
-      await entityCtx.updateAccounts([customAccount], props.selectedEntity)
-      handleClick()
-    }
-  }}
->
-  Save
-</button>
-
-
+                        if (accountDetails.length > 0 && typeof props.selectedEntity === "number") {
+                          await entityCtx.updateAccounts([customAccount], props.selectedEntity)
+                          handleClick()
+                        }
+                      }}
+                    >
+                      Save
+                    </button>
+                    
                     <button type="button" className="m-5 w-full rounded-lg bg-gradient-to-r from-gray-200 to-gray-100 py-2 shadow-inner" onClick={handleClick}>Cancel</button>
                   </div>
                 </>
               )}
-
             </div>
           </div>
         </div>
