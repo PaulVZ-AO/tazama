bfbd2c9551c8be0d7da5f56a50e52f1d670f0083
Oliver Vermeulen <127955247+Oliver-AO-GH@users.noreply.github.com>
2024-08-13 15:25:50 +0200

    removed unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import axios from "axios"
-// import { Codec, connect, NatsConnection, StringCodec } from "nats.ws"
 import { useContext, useEffect, useState } from "react"
 import { DebtorDevice } from "components/Device/Debtor"
 import { Modal } from "components/Modal/Modal"
@@ -14,133 +13,10 @@ import EntityContext from "store/entities/entity.context"
 const Web = () => {
   const [hoveredRule, setHoveredRule] = useState<any>(null)
   const [hoveredType, setHoveredType] = useState<any>(null)
-  // const [nats, setNats] = useState<NatsConnection>()
   const [showModal, setModal] = useState(false)
   const entityCtx = useContext(EntityContext)
 
-  // useEffect(() => {
-  //   const sc: Codec<string> = StringCodec()
-  //   ;(async () => {
-  //     const nc = await connect({
-  //       // servers: ["wss://demo.nats.io:8443"],
-  //       servers: ["wss://nats:4222"],
-  //     })
-  //     setNats(nc)
-  //     console.log("connected to NATS")
-  //     // const sub = nc.subscribe("echo")
-  //     const pacs008Sub = nc.subscribe("sub-rule-901@1.0.0")
-
-  //     const connected = nc.subscribe("connection")
-
-  //     const handle = (msg: any) => {
-  //       console.log(`Received a request: ${sc.decode(msg.data)}`)
-  //       msg.respond(msg.data)
-  //     }
-
-  //     // Wait to receive messages from the subscription and handle them
-  //     // asynchronously..
-  //     ;(async () => {
-  //       for await (const msg of connected) handle(msg)
-  //     })()
-  //     ;(async () => {
-  //       for await (const msg of pacs008Sub) handle(msg)
-  //     })()
-  //     // Now we can send a couple of requests to that subject. Note how we
-  //     // are encoding the string data on request and decoding the reply
-  //     // message data.
-
-  //     let connection = await nc.request("connection", sc.encode("Demo App Connected"))
-  //     console.log(`Received a reply: ${sc.decode(connection.data)}`)
-  //   })()
-
-  //   return () => {
-  //     nats?.drain()
-  //     console.log("closed NATS connection")
-  //   }
-  // }, [])
-
-  const handleRuleMouseEnter = (type: any) => {
-    setHoveredType(null) // fallback if stats is stuck
-    setHoveredRule(type)
-  }
-
-  const handleRuleMouseLeave = () => {
-    setHoveredRule(null)
-    setHoveredType(null) // fallback if stats is stuck
-  }
-
-  const handleTypeMouseEnter = (type: any) => {
-    setHoveredRule(null) // fallback if stats is stuck
-    setHoveredType(type)
-  }
-
-  const handleTypeMouseLeave = () => {
-    setHoveredRule(null) // fallback if stats is stuck
-    setHoveredType(null)
-  }
-
-  function RuleRow(props: any) {
-    let t = props.rule.t.map(function (type: any) {
-      return type.t
-    })
-
-    let typeRel = ""
-
-    if (hoveredType) {
-      for (let index = 0; index < hoveredType.r.length; index++) {
-        if (hoveredType.r[index].r === props.rule.r) {
-          typeRel = "bg-gray-200"
-        }
-      }
-    }
-
-    return (
-      <li
-        className={`flex rounded-md px-2 hover:bg-gray-200 ${typeRel}`}
-        data-type={t}
-        key={`r-${props.rule.r}`}
-        onMouseEnter={() => handleRuleMouseEnter(props.rule)}
-        onMouseLeave={handleRuleMouseLeave}
-      >
-        <StatusIndicator colour={props.rule.s} /> &nbsp;
-        {props.rule.v}
-      </li>
-    )
-  }
-
-  function TypeRow(props: any) {
-    let r = props.rule.r.map(function (type: any) {
-      return type.r
-    })
-
-    let ruleRel = ""
-
-    if (hoveredRule) {
-      for (let index = 0; index < hoveredRule.t.length; index++) {
-        if (hoveredRule.t[index].t === props.rule.t) {
-          ruleRel = "bg-gray-200"
-        }
-      }
-    }
-
-    return (
-      <li
-        className={`flex rounded-md px-2 hover:bg-gray-200 ${ruleRel}`}
-        data-rule={r}
-        key={`t-${props.rule.t}`}
-        onMouseEnter={() => handleTypeMouseEnter(props.rule)}
-        onMouseLeave={handleTypeMouseLeave}
-      >
-        <StatusIndicator colour={props.rule.s} /> &nbsp;
-        {props.rule.v}
-      </li>
-    )
-  }
-
   function RuleResult() {
-    // if (hoveredRule == null)
-    // return(null);
-
     return (
       <div className="rounded-xl p-5 shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)]">
         <h3 className="text-center uppercase">Rule Results</h3>
@@ -161,9 +37,6 @@ const Web = () => {
   }
 
   function TypeResult() {
-    // if (hoveredType == null)
-    //   return(null);
-
     return (
       <div className="rounded-xl p-5 shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)]">
         <h3 className="text-center uppercase">Type Results</h3>
@@ -173,36 +46,14 @@ const Web = () => {
           {hoveredType ? (hoveredType.s === "g" ? "true" : "false") : ""} 600
         </div>
         <div className="mb-2 flex p-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-            />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
           </svg>
           500
         </div>
         <div className="mb-2 flex p-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M10.05 4.575a1.575 1.575 0 1 0-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 0 1 3.15 0v1.5m-3.15 0 .075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 0 1 3.15 0V15M6.9 7.575a1.575 1.575 0 1 0-3.15 0v8.175a6.75 6.75 0 0 0 6.75 6.75h2.018a5.25 5.25 0 0 0 3.712-1.538l1.732-1.732a5.25 5.25 0 0 0 1.538-3.712l.003-2.024a.668.668 0 0 1 .198-.471 1.575 1.575 0 1 0-2.228-2.228 3.818 3.818 0 0 0-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0 1 16.35 15m.002 0h-.002"
-            />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10.05 4.575a1.575 1.575 0 1 0-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 0 1 3.15 0v1.5m-3.15 0 .075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 0 1 3.15 0V15M6.9 7.575a1.575 1.575 0 1 0-3.15 0v8.175a6.75 6.75 0 0 0 6.75 6.75h2.018a5.25 5.25 0 0 0 3.712-1.538l1.732-1.732a5.25 5.25 0 0 0 1.538-3.712l.003-2.024a.668.668 0 0 1 .198-.471 1.575 1.575 0 1 0-2.228-2.228 3.818 3.818 0 0 0-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0 1 16.35 15m.002 0h-.002" />
           </svg>
           none
         </div>
@@ -217,10 +68,6 @@ const Web = () => {
   const [error, setError] = useState(null)
   const [selectedEntity, setSelectedEntity] = useState<number>(0)
   const [selectedCreditorEntity, setSelectedCreditorEntity] = useState<number>(0)
-
-  const handleAccountChange = async (creditorIdx: number, accountIdx: number) => {
-    await entityCtx.setCreditorAccountPacs008(creditorIdx, accountIdx)
-  }
 
   useEffect(() => {
     setSelectedEntity(entityCtx.selectedDebtorEntity.debtorSelectedIndex || 0)
@@ -255,21 +102,11 @@ const Web = () => {
         setLoading(false)
       })
   }, [])
-  useEffect(() => {
-    console.log("SELECTED ENTITY: ", selectedEntity)
-  }, [selectedEntity])
 
-  useEffect(() => {
-    console.log("SELECTED CREDITOR ENTITY: ", selectedCreditorEntity)
-  }, [selectedCreditorEntity])
-
-  useEffect(() => {
-    console.log("DEBTORS: ", entityCtx.entities)
-  }, [entityCtx.entities])
-
-  useEffect(() => {
-    console.log("CREDITORS: ", entityCtx.creditorEntities)
-  }, [entityCtx.creditorEntities])
+  useEffect(() => {console.log("SELECTED ENTITY: ", selectedEntity)}, [selectedEntity])
+  useEffect(() => {console.log("SELECTED CREDITOR ENTITY: ", selectedCreditorEntity)}, [selectedCreditorEntity])
+  useEffect(() => {console.log("DEBTORS: ", entityCtx.entities)}, [entityCtx.entities])
+  useEffect(() => {console.log("CREDITORS: ", entityCtx.creditorEntities)}, [entityCtx.creditorEntities])
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error: {error}</p>
@@ -281,7 +118,6 @@ const Web = () => {
         <div className="col-span-2">
           <div className="flex flex-wrap justify-center rounded-lg p-5 shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)]">
             <div className="mb-5 text-center text-xl">Debtors</div>
-
             <Profile
               colour={!entityCtx.entities[0] ? "text-gray-300" : "text-blue-500"}
               entity={entityCtx.entities[0]?.Entity}
@@ -293,7 +129,6 @@ const Web = () => {
               addAccount={async () => {
                 await entityCtx.createEntityAccount(0)
                 await entityCtx.selectDebtorEntity(0, 0)
-                // await entityCtx.selectDebtorEntity
               }}
             />
             <Profile
@@ -341,16 +176,9 @@ const Web = () => {
         {/* Device transactions */}
         <div className="col-span-8">
           <div className="grid grid-cols-12 gap-1">
-            <div className="col-span-4">
-              <DebtorDevice selectedEntity={selectedEntity} isDebtor={true} />
-            </div>
-            <div className="col-span-4 flex items-center justify-between px-5">
-              <ProcessIndicator />
-            </div>
-            <div className="col-span-4">
-              <DebtorDevice selectedEntity={selectedCreditorEntity} isDebtor={false} />
-              {/* <Image src="/device.svg" height="200" width="200" className="text-center" alt="" priority={true} /> */}
-            </div>
+            <div className="col-span-4"><DebtorDevice selectedEntity={selectedEntity} isDebtor={true} /></div>
+            <div className="col-span-4 flex items-center justify-between px-5"><ProcessIndicator /></div>
+            <div className="col-span-4"><DebtorDevice selectedEntity={selectedCreditorEntity} isDebtor={false} /></div>
           </div>
         </div>
 
@@ -370,13 +198,6 @@ const Web = () => {
                   entityCtx.creditorEntities[0]?.CreditorAccounts.length !== -1 &&
                   entityCtx.creditorEntities[0]?.CreditorAccounts.length !== undefined
                 ) {
-                  // let idx = 0
-                  // if (entityCtx.creditorEntities[0]?.CreditorAccounts.length !== -1) {
-                  //   // if (entityCtx.entities[0]?.Accounts.length !== -1) {
-                  //   // idx = entityCtx.entities[0]?.Accounts.length - 1
-                  //   idx = entityCtx.creditorEntities[0]?.CreditorAccounts.length - 1
-                  // }
-
                   await entityCtx.setCreditorAccountPacs008(0, idx)
                 }
               }}
@@ -384,15 +205,12 @@ const Web = () => {
               selectedEntity={selectedCreditorEntity}
               addAccount={async () => {
                 await entityCtx.createCreditorEntityAccount(0)
-                // if (entityCtx.entities[0]?.Accounts.length !== -1 && entityCtx.entities[0]?.Accounts !== undefined) {
                 if (
                   entityCtx.creditorEntities[0]?.CreditorAccounts.length !== -1 &&
                   entityCtx.creditorEntities[0]?.CreditorAccounts.length !== undefined
                 ) {
                   let idx = 0
                   if (entityCtx.creditorEntities[0]?.CreditorAccounts.length !== -1) {
-                    // if (entityCtx.entities[0]?.Accounts.length !== -1) {
-                    // idx = entityCtx.entities[0]?.Accounts.length - 1
                     idx = entityCtx.creditorEntities[0]?.CreditorAccounts.length - 1
                   }
                   await entityCtx.setCreditorAccountPacs008(0, idx)
@@ -412,17 +230,9 @@ const Web = () => {
                   entityCtx.creditorEntities[1]?.CreditorAccounts.length !== -1 &&
                   entityCtx.creditorEntities[1]?.CreditorAccounts.length !== undefined
                 ) {
-                  // let idx = 0
-                  // if (entityCtx.creditorEntities[1]?.CreditorAccounts.length !== -1) {
-                  // if (entityCtx.entities[1]?.Accounts.length !== -1) {
-                  // idx = entityCtx.entities[1]?.Accounts.length - 1
-                  // idx = entityCtx.creditorEntities[1]?.CreditorAccounts.length - 1
-                  // }
-                  // await entityCtx.selectCreditorEntity(1, idx)
                   await entityCtx.setCreditorAccountPacs008(1, idx)
                 }
               }}
-              // setSelectedEntity={() => setSelectedCreditorEntity(1)}
               selectedEntity={selectedCreditorEntity}
               addAccount={async () => {
                 await entityCtx.createCreditorEntityAccount(1)
@@ -432,8 +242,6 @@ const Web = () => {
                 ) {
                   let idx = 0
                   if (entityCtx.creditorEntities[1]?.CreditorAccounts.length !== -1) {
-                    // if (entityCtx.entities[0]?.Accounts.length !== -1) {
-                    // idx = entityCtx.entities[0]?.Accounts.length - 1
                     idx = entityCtx.creditorEntities[1]?.CreditorAccounts.length - 1
                   }
                   await entityCtx.setCreditorAccountPacs008(1, idx)
@@ -457,8 +265,6 @@ const Web = () => {
                 ) {
                   let idx = 0
                   if (entityCtx.creditorEntities[2]?.CreditorAccounts.length !== -1) {
-                    // if (entityCtx.entities[0]?.Accounts.length !== -1) {
-                    // idx = entityCtx.entities[0]?.Accounts.length - 1
                     idx = entityCtx.creditorEntities[2]?.CreditorAccounts.length - 1
                   }
                   await entityCtx.setCreditorAccountPacs008(2, idx)
@@ -482,8 +288,6 @@ const Web = () => {
                 ) {
                   let idx = 0
                   if (entityCtx.creditorEntities[3]?.CreditorAccounts.length !== -1) {
-                    // if (entityCtx.entities[0]?.Accounts.length !== -1) {
-                    // idx = entityCtx.entities[0]?.Accounts.length - 1
                     idx = entityCtx.creditorEntities[3]?.CreditorAccounts.length - 1
                   }
                   await entityCtx.setCreditorAccountPacs008(3, idx)
@@ -497,13 +301,8 @@ const Web = () => {
       <div className="grid grid-cols-12 gap-5 pt-10">
         {/* CRSP */}
         <div className="col-span-2 rounded-md shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)]">
-          <h2 className="mb-5 rounded-t-lg bg-gradient-to-r from-gray-100 to-gray-200 py-5 text-center uppercase shadow-lg">
-            Event director
-          </h2>
-
-          <div className="flex min-h-80 items-center justify-center">
-            <StatusIndicator large={true} />
-          </div>
+          <h2 className="mb-5 rounded-t-lg bg-gradient-to-r from-gray-100 to-gray-200 py-5 text-center uppercase shadow-lg">Event director</h2>
+          <div className="flex min-h-80 items-center justify-center"><StatusIndicator large={true} /></div>
         </div>
 
         {/* Rules */}
@@ -523,18 +322,13 @@ const Web = () => {
                   ))}
               </div>
             </div>
-            <div className="col-span-6 px-5">
-              <RuleResult />
-            </div>
+            <div className="col-span-6 px-5"><RuleResult /></div>
           </div>
         </div>
 
         {/* Typologies */}
         <div className="col-span-4 rounded-lg shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)]">
-          <h2 className="mb-5 rounded-t-lg bg-gradient-to-r from-gray-100 to-gray-200 py-5 text-center uppercase shadow-lg">
-            Typologies
-          </h2>
-
+          <h2 className="mb-5 rounded-t-lg bg-gradient-to-r from-gray-100 to-gray-200 py-5 text-center uppercase shadow-lg">Typologies</h2>
           <div className="grid grid-cols-12">
             <div className="col-span-6">
               <div className="grid grid-cols-3 px-5">
@@ -547,21 +341,14 @@ const Web = () => {
                   ))}
               </div>
             </div>
-            <div className="col-span-6 px-5">
-              <TypeResult />
-            </div>
+            <div className="col-span-6 px-5"><TypeResult /></div>
           </div>
         </div>
 
         {/* Tadproc */}
         <div className="col-span-2 rounded-lg shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)]">
-          <h2 className="mb-5 rounded-t-lg bg-gradient-to-r from-gray-100 to-gray-200 py-5 text-center uppercase shadow-lg">
-            Tadproc
-          </h2>
-
-          <div className="flex min-h-80 items-center justify-center">
-            <StatusIndicator large={true} />
-          </div>
+          <h2 className="mb-5 rounded-t-lg bg-gradient-to-r from-gray-100 to-gray-200 py-5 text-center uppercase shadow-lg">Tadproc</h2>
+          <div className="flex min-h-80 items-center justify-center"><StatusIndicator large={true} /></div>
         </div>
       </div>
 
--- components/ProfileCreditor/ProfileCreditor.tsx
+++ components/ProfileCreditor/ProfileCreditor.tsx
@@ -61,11 +61,7 @@ const CreditorAccountsComponent = ({
     }
   }
   return (
-    <button
-      onClick={() => {
-        handleClick()
-      }}
-    >
+    <button onClick={() => {handleClick()}}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`size-6 ${colour}`}>
         <path d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625Z" />
         <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
@@ -78,19 +74,6 @@ export const CreditorProfile = ({ ...props }: ProfileProps) => {
   const entityCtx = useContext(EntityContext)
   const [selectedAccountIndex, setSelectedAccountIndex] = useState(0)
 
-  // useEffect(() => {
-  //   if (entityCtx.creditorEntities.length > 0) {
-  //     entityCtx.setCreditorAccountPacs008(props.selectedEntity, selectedAccountIndex)
-  //   }
-  // }, [selectedAccountIndex])
-
-  // useEffect(() => {
-  //   if (entityCtx.creditorEntities.length > 0) {
-  //     entityCtx.setCreditorPacs008(props.selectedEntity)
-  //   }
-  // }, [props.selectedEntity])
-
-  // useEffect(() => {}, [entityCtx.entities])
   let reverse = ""
   if (props.reverse) {
     reverse = "flex-row-reverse text-right"
@@ -98,43 +81,22 @@ export const CreditorProfile = ({ ...props }: ProfileProps) => {
 
   const CreditorAccounts = props.creditorAccounts?.map((account, index) => {
     return (
-      <CreditorAccountsComponent
-        key={crypto.randomUUID().replaceAll("-", "")}
-        index={index}
-        selected={selectedAccountIndex}
-        setSelected={setSelectedAccountIndex}
-        selectedEntityIndex={props.index}
-        setSelectedEntity={props.setSelectedEntity}
-      />
+      <CreditorAccountsComponent key={crypto.randomUUID().replaceAll("-", "")} index={index} selected={selectedAccountIndex} setSelected={setSelectedAccountIndex} selectedEntityIndex={props.index} setSelectedEntity={props.setSelectedEntity} />
     )
   })
 
   return (
-    <div
-      className={`mb-7 rounded-lg bg-gradient-to-r from-gray-200 to-gray-100 px-3 py-1 shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)] ${props.colour} flex w-full justify-between ${reverse}`}
-    >
+    <div className={`mb-7 rounded-lg bg-gradient-to-r from-gray-200 to-gray-100 px-3 py-1 shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)] ${props.colour} flex w-full justify-between ${reverse}`}>
       <button
         className="text-black"
         onClick={async () => {
           if (props.entity !== undefined) {
-            // props.setSelectedEntity(selectedAccountIndex)
             props.setModalVisible(true)
           }
         }}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="size-10"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-          />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-10">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
         </svg>
       </button>
 
@@ -143,35 +105,16 @@ export const CreditorProfile = ({ ...props }: ProfileProps) => {
           console.log("ATTEMPTING PACS008 FROM CREDITORS SIDE")
           props.setSelectedEntity(props.index)
           console.log("SELECTED INDEX: " + props.selectedEntity)
-          // console.log("ABOUT TO CALL THE setCreditorPacs008")
           if (entityCtx.creditorEntities[props.selectedEntity] !== undefined) {
             await entityCtx.selectCreditorEntity(props.index, 0)
-            // await entityCtx.setCreditorPacs008(props.selectedEntity)
           }
         }}
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-          <path
-            fillRule="evenodd"
-            d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
-            clipRule="evenodd"
-          />
+          <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
         </svg>
       </button>
       {CreditorAccounts}
-
-      {/* <button>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-          <path d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625Z" />
-          <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
-        </svg>
-      </button>
-      <button>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-          <path d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625Z" />
-          <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
-        </svg>
-      </button> */}
 
       {props?.creditorAccounts !== null &&
         props.creditorAccounts !== undefined &&
@@ -204,11 +147,7 @@ export const CreditorProfile = ({ ...props }: ProfileProps) => {
         }}
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-          <path
-            fillRule="evenodd"
-            d="M14.615 1.595a.75.75 0 0 1 .359.852L12.982 9.75h7.268a.75.75 0 0 1 .548 1.262l-10.5 11.25a.75.75 0 0 1-1.272-.71l1.992-7.302H3.75a.75.75 0 0 1-.548-1.262l10.5-11.25a.75.75 0 0 1 .913-.143Z"
-            clipRule="evenodd"
-          />
+          <path fillRule="evenodd" d="M14.615 1.595a.75.75 0 0 1 .359.852L12.982 9.75h7.268a.75.75 0 0 1 .548 1.262l-10.5 11.25a.75.75 0 0 1-1.272-.71l1.992-7.302H3.75a.75.75 0 0 1-.548-1.262l10.5-11.25a.75.75 0 0 1 .913-.143Z" clipRule="evenodd" />
         </svg>
       </button>
     </div>
